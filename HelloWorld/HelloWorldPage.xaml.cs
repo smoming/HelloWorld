1ad3e5b9829dd0313fa110abf0cdf0781f8f5c9e
smoming <smoming1031@gmail.com>
2017-04-07 23:56:41 +0800

    Xamarin First App
    Xamarin First App

---
+++ HelloWorld/HelloWorldPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Xamarin.Forms;
+//using Xamarin.Forms.Xaml;
+
+//debug only this namespace
+//[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace HelloWorld
+{
+	//debug only this class
+	//[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HelloWorldPage : ContentPage
+	{
+		public HelloWorldPage()
+		{
+			InitializeComponent();
+
+			//if (Device.OS == TargetPlatform.Android)
+			//{
+			//}
+			//else if (Device.OS == TargetPlatform.Android)
+			//{
+			//}
+
+			//Padding = new OnPlatform<Thickness>()
+			//{
+			//	iOS = new Thickness(0, 20, 0, 0),
+			//	Android = new Thickness(0)
+			//};
+
+			//label1.Text = "Hello Ming!!";
+
+			//Content = new Grid
+			//{
+			//	ColumnDefinitions = new ColumnDefinitionCollection() {
+			//		new ColumnDefinition() { Width = new GridLength(100, GridUnitType.Auto) },
+			//		new ColumnDefinition() { Width = new GridLength(100, GridUnitType.Auto) },
+			//		new ColumnDefinition() { Width = new GridLength(100, GridUnitType.Auto) }
+			//	},
+			//	RowDefinitions = new RowDefinitionCollection()
+			//	{
+			//		new RowDefinition() { Height = new GridLength(100, GridUnitType.Auto) },
+			//		new RowDefinition() { Height = new GridLength(100, GridUnitType.Auto) },
+			//		new RowDefinition() { Height = new GridLength(100, GridUnitType.Auto) }
+			//	}
+			//};
+
+			//Content = new Label
+			//{
+			//	Text = "00",
+			//	BackgroundColor = Color.Yellow
+
+			//};
+		}
+	}
+}
